2b88e7bc37b5e02ba6b8874616bb3686e99d8c26
Ryan Lee <12857462+ryanlee92@users.noreply.github.com>
2025-09-15 23:51:37 +0900

    Update in_app_webview.cpp

--- flutter_inappwebview_windows/windows/in_app_webview/in_app_webview.cpp
+++ flutter_inappwebview_windows/windows/in_app_webview/in_app_webview.cpp
@@ -2618,9 +2618,10 @@ namespace flutter_inappwebview_plugin
     HWND parentHwnd = nullptr;
     if (succeededOrLog(webViewController->get_ParentWindow(&parentHwnd)) && parentHwnd != nullptr) {
       LONG_PTR exStyle = GetWindowLongPtr(parentHwnd, GWL_EXSTYLE);
-      SetWindowLongPtr(parentHwnd, GWL_EXSTYLE, exStyle | WS_EX_TRANSPARENT);
+      exStyle |= WS_EX_TRANSPARENT | WS_EX_NOACTIVATE;
+      SetWindowLongPtr(parentHwnd, GWL_EXSTYLE, exStyle);
       SetWindowPos(parentHwnd, HWND_NOTOPMOST, 0, 0, 0, 0,
-        SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE | SWP_NOOWNERZORDER);
+        SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE | SWP_NOOWNERZORDER | SWP_FRAMECHANGED);
     }
 
     return true;
